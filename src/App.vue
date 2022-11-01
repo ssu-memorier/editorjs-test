bcf55a9ea2dd1e943c89692ae61abd8a4bceccdd
dev2820 <second28200@gmail.com>
2022-11-01 10:37:52 +0900

    app.vue 내용 초기화

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h2>editor js test</h2>
+    <menu></menu>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
-
+<script setup lang="ts"></script>
 <style scoped>
 .logo {
   height: 6em;
