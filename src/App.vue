0774119f07367ad132a9bb1b19231798994d4136
dev2820 <second28200@gmail.com>
2022-11-01 10:51:14 +0900

    editorjs 컴포넌트 생성 & 연결

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <h2>editor js test</h2>
     <menu></menu>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <editor-js></editor-js>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EditorJs from "@/components/EditorJs.vue";
+</script>
 <style scoped></style>
---
+++ src/components/EditorJs.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="editorjs"></div>
+</template>
+<script setup lang="ts">
+import EditorJS from "@editorjs/editorjs";
+
+const editor = new EditorJS({
+  holder: "editorjs",
+});
+</script>
